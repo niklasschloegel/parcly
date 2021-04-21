734f84b1464a9dbb03cde3508e14bf8decb83609
Niklas Schlögel <niklasschloegel@student.hs-rm.de>
2021-04-21 20:17:13 +0200

    fixing "error message shown twice" bug

--- cmd/root.go
+++ cmd/root.go
@@ -68,7 +68,7 @@ The API Key can be provided in three ways:
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
-	cobra.CheckErr(rootCmd.Execute())
+	rootCmd.Execute()
 }
 
 func init() {
